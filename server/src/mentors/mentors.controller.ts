4f4dbb72aedae7b7117604a0dcbf7b6ad9dd6477
Amir Alsayed <amyralsyd367@gmail.com>
2026-06-18 21:06:37 +0300

    feat: add get mentor details requist for testign student frontend ui

--- server/src/mentors/mentors.controller.ts
+++ server/src/mentors/mentors.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import { MentorsService } from './mentors.service';
 
 @Controller('mentors')
-export class MentorsController {}
+export class MentorsController {
+  constructor(private readonly mentorsService: MentorsService) {}
+
+  @Get(':id')
+  async findOne(@Param('id', ParseIntPipe) id: number) {
+    return await this.mentorsService.findOne(id);
+  }
+}
--- server/src/mentors/mentors.service.ts
+++ server/src/mentors/mentors.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
-export class MentorsService {}
+export class MentorsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async findOne(id: number) {
+    const mentor = await this.prisma.mentorProfile.findUnique({
+      where: { id },
+      include: {
+        stack: true,
+        availability: true,
+      },
+    });
+
+    if (!mentor) {
+      throw new NotFoundException(`Mentor with ID ${id} not found.`);
+    }
+
+    return {
+      success: true,
+      data: mentor,
+    };
+  }
+}
